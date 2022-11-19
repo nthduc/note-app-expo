f7a874f59ac9d15319544d0b559f4e1a5aeee0ce
nthduc <nguyenthaiduc0212@gmail.com>
2022-11-19 21:30:18 +0700

    set up app

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { NativeBaseProvider, Box } from 'native-base';
+import Home from './src/Home';
+import Note from './src/Note';
+import About from './src/About';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='Home' component={Home} />
+          <Stack.Screen name='Note' component={Note} />
+          <Stack.Screen name='About' component={About} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
 
